57a332a1f657d83f3cbfa255df956493835b5495
SUSHIL CHOUDHARY <s4SUSHIL@yahoo.com>
2019-08-12 15:33:44 +0100

    updated gradle dependencies, App class and set up Hsql

--- src/main/java/com/creditsuisse/app/Application.java
+++ src/main/java/com/creditsuisse/app/Application.java
@@ -1,5 +1,8 @@
 package com.creditsuisse.app;
 
+import java.io.File;
+import java.security.InvalidParameterException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -11,6 +14,19 @@ public class Application {
 	private static final Logger log = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) {
+		if (args.length == 0) {
+			log.error("Json file path not provided.");
+		} else {
+			String logFilePath = args[0];
+			File inFile = new File(logFilePath);
+			if (!inFile.exists()) {
+				log.error("Json file does not exist. Provided Path:" + logFilePath);
+				throw new InvalidParameterException("Missing log filePath argument");
+			}
+			log.info("PATH:" + logFilePath);
+			System.setProperty("logFilePath", logFilePath);
+		}
+		
 		SpringApplication.run(Application.class, args);
 	}
 
